3645e6cf9a4b5bcb73411f66fba1a888739a1c3d
Adriana Rivera <letsgotravel007-github@yahoo.com>
2017-01-22 00:16:39 +0000

    Part 2 almost completed. Beautify

--- server.py
+++ server.py
@@ -4,12 +4,34 @@ from flask_debugtoolbar import DebugToolbarExtension
 
 app = Flask(__name__)
 
+JOBS = [
+    'Software Engineer', 'QA Engineer', 'Product Manager']
 
 # Required to use Flask sessions and the debug toolbar
 app.secret_key = "ABC"
 
 
-# YOUR ROUTES GO HERE
+@app.route('/')
+def serve_home():
+    return render_template("templates/index.html")
+
+@app.route('/application-form')
+def application_form():
+    return render_template("templates/application-form.html", jobs = JOBS)
+
+@app.route('/application-success', methods=["POST"] )
+def application_success():
+
+    firstname = request.form.get("firstname")
+    lastname = request.form.get("lastname")
+    jobtype = request.form.get("jobtype")
+    salary = request.form.get("salary")
+    
+    # salary = request.args.get("salary")   #args or form?
+    
+    return render_template("templates/application-success.html", success_firstname = firstname,
+                                                                success_lastname = lastname,
+                                                                success_jobtype = jobtype)
 
 
 if __name__ == "__main__":
