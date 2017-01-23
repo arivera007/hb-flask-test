afd7924c1a9c337710f78dbeddf29ab8f90a7aad
vagrant <vagrant@vagrant.vm>
2017-01-23 04:12:41 +0000

    Changes made in iMac. Par 1 done

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, render_template
 from flask_debugtoolbar import DebugToolbarExtension
+import jinja2
 
 
 app = Flask(__name__)
@@ -10,18 +11,26 @@ JOBS = [
 # Required to use Flask sessions and the debug toolbar
 app.secret_key = "ABC"
 
+app.jinja_env.undefined = jinja2.StrictUndefined
+app.jinja_env.auto_reload = True
 
 @app.route('/')
 def serve_home():
-    return render_template("templates/index.html")
+    return render_template("index.html")
+    # return "templates/index.html"
 
 @app.route('/application-form')
 def application_form():
-    return render_template("templates/application-form.html", jobs = JOBS)
+    return render_template("application-form.html", jobs = JOBS)
 
 @app.route('/application-success', methods=["POST"] )
 def application_success():
 
+    # firstname = request.args.get("firstname")
+    # lastname = request.args.get("lastname")
+    # jobtype = request.args.get("jobtype")
+    # salary = request.args.get("salary")
+    
     firstname = request.form.get("firstname")
     lastname = request.form.get("lastname")
     jobtype = request.form.get("jobtype")
@@ -29,9 +38,10 @@ def application_success():
     
     # salary = request.args.get("salary")   #args or form?
     
-    return render_template("templates/application-success.html", success_firstname = firstname,
+    return render_template("application-response.html", success_firstname = firstname,
                                                                 success_lastname = lastname,
-                                                                success_jobtype = jobtype)
+                                                                success_jobtype = jobtype,
+                                                                success_salary = salary)
 
 
 if __name__ == "__main__":
